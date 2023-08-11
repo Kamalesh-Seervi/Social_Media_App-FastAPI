1f5814a2a3b16089f2c402892891f45fa0e1788e
kd14 <kd14@ford.com>
2023-08-11 22:35:07 +0530

    Changed to orm based get/post/put/delete

--- app/main.py
+++ app/main.py
@@ -12,7 +12,6 @@ from .database import engine,get_db
 from . import models
 #import models.py
 
-
 models.base.metadata.create_all(bind=engine)
 
 
@@ -39,61 +38,98 @@ while True:
 # my_posts = [{"title": "title of of post 1", "content": "content of the post", "id": 1}, {
 #     "title": "favorite foods", "content": "i like pizza", "id": 2
 # }]
-
-
+    
 @app.get("/")  # path
 def read_root():
     return {"message": "hello folks"}
 
-@app.get("ormtest")
-def betatest( db: Session = Depends(get_db)):
-    return{"Status":"success"}
+@app.get("/test")
+def orm_test(db:Session=Depends(get_db)):
+    post=db.query(models.Post).all()
+    return {"message":post }
 
+
+# @app.get("/posts")
+# def get_posts():
+#     cursor.execute("""SELECT * FROM posts""")
+#     posts= cursor.fetchall()
+#     # print(posts)
+#     return {"message": posts}
 
 @app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    posts= cursor.fetchall()
-    # print(posts)
+def get_posts(db:Session=Depends(get_db)):
+    posts=db.query(models.Post).all()
     return {"message": posts}
+
 
 # access based on each property
 # fix a datatype so now you get a bug if specified datatype not followed
 
 
+# @app.post("/posts",status_code=status.HTTP_201_CREATED)
+# def createpost(Post: Post):
+#     cursor.execute("""INSERT INTO posts (title,content,published) VALUES (%s,%s,%s) RETURNING * """,(Post.title,Post.content,Post.published))
+#     new_post= cursor.fetchone()
+#     conn.commit()
+#     return {"message":new_post}
+
 @app.post("/posts",status_code=status.HTTP_201_CREATED)
-def createpost(Post: Post):
-    cursor.execute("""INSERT INTO posts (title,content,published) VALUES (%s,%s,%s) RETURNING * """,(Post.title,Post.content,Post.published))
-    new_post= cursor.fetchone()
-    conn.commit()
+def createpost(Post: Post,db:Session=Depends(get_db)):
+    new_post=models.Post(**Post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"message":new_post}
 
 
+# @app.get("/posts/{id}")
+# def get_post(id:int):
+#     cursor.execute("""SELECT * from posts WHERE id=%s""",(str(id),))
+#     post=cursor.fetchone()
+#     if not post :
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id:{id} was not found")
+#     return {"Post_Details": post}
+
 @app.get("/posts/{id}")
-def get_post(id:int):
-    cursor.execute("""SELECT * from posts WHERE id=%s""",(str(id),))
-    post=cursor.fetchone()
+def get_post(id:int,db:Session=Depends(get_db)):
+    post=db.query(models.Post).filter(models.Post.id == id).first()
     if not post :
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id:{id} was not found")
     return {"Post_Details": post}
 
+# @app.delete("/posts/{id}")
+# def del_post(id:int):
+#     cursor.execute("""DELETE from posts WHERE id=%s RETURNING *""",(str(id),))
+#     delete=cursor.fetchone()
+#     conn.commit()
+#     if not delete:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id:{id} was not found")
+#     return {"Post_Details": delete}
+
 @app.delete("/posts/{id}")
-def del_post(id:int):
-    cursor.execute("""DELETE from posts WHERE id=%s RETURNING *""",(str(id),))
-    delete=cursor.fetchone()
-    conn.commit()
-    if not delete:
+def del_post(id:int,db:Session=Depends(get_db)):
+    delete=db.query(models.Post).filter(models.Post.id==id)
+
+    if delete.first()==None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id:{id} was not found")
+    Post.delete(synchronize_session=False)
+    db.commit()
     return {"Post_Details": delete}
 
-@app.put("/posts/{id}")
-def update_post(id:int,Post:Post):
-    cursor.execute("""UPDATE posts SET title = %s,content=%s,published=%s WHERE id =%s RETURNING *""",(Post.title,Post.content,Post.published,(str(id))))
-    update=cursor.fetchone()
-    conn.commit()
-    if not update:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id:{id} was not found")
-    return {"Post_Details": update}
+# @app.put("/posts/{id}")
+# def update_post(id:int,Post:Post):
+#     cursor.execute("""UPDATE posts SET title = %s,content=%s,published=%s WHERE id =%s RETURNING *""",(Post.title,Post.content,Post.published,(str(id))))
+#     update=cursor.fetchone()
+#     conn.commit()
+#     if not update:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id:{id} was not found")
+#     return {"Post_Details": update}
     
-
-
+@app.put("/posts/{id}")
+def update_post(id:int,Posts:Post,db:Session=Depends(get_db)):
+    update=db.query(models.Post).filter(models.Post.id ==id).first()
+    if update==None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id:{id} was not found")
+    update.update(Posts.dict(),synchronize_session=False)
+    db.commit()
+    return {"Post_Details": "success"}
